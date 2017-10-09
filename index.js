56ab27e231be2ee209a455fb73fcb2b743b2cee7
enlore <n.e.lorenson@gmail.com>
2017-10-09 15:35:42 -0500

    Index file to surface adapters

---
+++ index.js
@@ -0,0 +1,5 @@
+/* jshint node: true, asi: true, laxcomma: true, esversion: 6 */
+'use strict'
+
+module.exports = require('./src/socks.js')
+module.exports.vue = require('./src/socks-vue-plugin.js')
